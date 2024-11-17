ae5b8a4441db9bb9e09e642cf3477bf7c55f4049
brian <b384li@uwaterloo.ca>
2024-11-16 23:43:52 -0500

    change env

--- hparam_search.py
+++ hparam_search.py
@@ -10,7 +10,7 @@ from utils import eval, create_model_optuna
 envs = ['highway-fast-v0', 'intersection-v0', 'racetrack-v0']
 agents = ['A2C', 'PPO', 'DQN']
 
-ENV = envs[1]
+ENV = envs[2]
 NUM_TRIALS = 20
 AGENT = agents[-1]
 TRAIN_TIMESTEPS = int(1e4)
